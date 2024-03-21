1a0651d003f1b9bbd4cda23b328afb3625c6aa87
Denysit <141247457+Denysit@users.noreply.github.com>
2024-03-22 01:11:37 +0200

    update

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,13 +1,24 @@
 import css from "./SearchBox.module.css";
+import { useDispatch } from "react-redux";
+import { changeFilter } from "../../redux/filtersSlice";
 
-export default function SearchBox({ inputValue, handleChange }) {
+
+export default function SearchBox() {
+    
+    const dispatch = useDispatch();
+
+    const handleChange = (event) => {
+        const inputValue = event.target.value;
+        dispatch(changeFilter(inputValue))
+
+    }
+
     return (
         <div className={css.searchBox}>
             <label htmlFor="name" className={css.label}>Search contact by name</label>
             <input
                 id="name"
                 type="text"
-                value={inputValue}
                 onChange={handleChange}
                 className={css.input}
             />
